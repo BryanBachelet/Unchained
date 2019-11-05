34962555bbd7d904f60f1104ece834cca4aea7be
Endeavolt <49842893+Endeavolt@users.noreply.github.com>
2019-11-05 14:59:23 +0100

    Erreur

--- Projet S3/Assets/JoliScript.cs
+++ Projet S3/Assets/JoliScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class JoliScript : MonoBehaviour
 {
+    public float connerie;
+    public int test;
     // Start is called before the first frame update
     void Start()
     {
-        
+        test = 2;
     }
 
     // Update is called once per frame
