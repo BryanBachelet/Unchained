962b88423f1d68d2a1a5b19aad4e375358235c7c
Endeavolt <49842893+Endeavolt@users.noreply.github.com>
2019-11-05 14:57:38 +0100

    Troisième fois

--- Projet S3/Assets/JoliScript.cs
+++ Projet S3/Assets/JoliScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class JoliScript : MonoBehaviour
 {
+    public int test;
     // Start is called before the first frame update
     void Start()
     {
-        
+        test = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void testFonc()
+    {
+        Debug.Log("1");
     }
 }
