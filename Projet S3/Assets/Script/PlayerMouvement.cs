52959ea9b781a9cfd9854817af05cb07ecf6a518
Endeavolt <49842893+Endeavolt@users.noreply.github.com>
2019-11-05 16:08:47 +0100

    Player Mouvement V1

---
+++ Projet S3/Assets/Script/PlayerMouvement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouvement : MonoBehaviour
+{
+    private float horizontal;
+    private float vertical;
+
+    public float speedOfDeplacement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        transform.position += new Vector3(horizontal, 0, vertical).normalized * speedOfDeplacement * Time.deltaTime;
+
+    }
+}
