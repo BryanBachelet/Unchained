3a9ca029c9335cd128120100c0e599e93a13b805
Endeavolt <49842893+Endeavolt@users.noreply.github.com>
2019-11-05 17:51:30 +0100

    Input Manager

--- Projet S3/Assets/Script/PlayerNumber.cs
+++ Projet S3/Assets/Script/PlayerNumber.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class PlayerNumber : MonoBehaviour
 {
+    public InputManager inputManager;
     public int playerNumber;
     public int manetteNumber;
 
+    public void Awake()
+    {
+        inputManager = FindObjectOfType<InputManager>();
+        if(playerNumber== 1)
+        {
+            manetteNumber = inputManager.controllerOne;
+        }
+        if(playerNumber == 2)
+        {
+            manetteNumber = inputManager.controllerTwo;
+        }
+    }
 
 }
