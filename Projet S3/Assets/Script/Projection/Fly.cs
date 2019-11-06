5ca9bc82cae2310f44fa4aa48aed92e2441ed537
Endeavolt <49842893+Endeavolt@users.noreply.github.com>
2019-11-06 13:42:52 +0100

    Projection V1

---
+++ Projet S3/Assets/Script/Projection/Fly.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    [HideInInspector] public Vector3 direction;
+    [HideInInspector] public float timeToFlight;
+    [HideInInspector] public float speedOfFight;
+    [HideInInspector] public float opportunityWindow;
+
+    public float compteurOfFlyTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (compteurOfFlyTime > timeToFlight)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            transform.position += direction * speedOfFight * Time.deltaTime;
+            compteurOfFlyTime += Time.deltaTime;
+        }
+    }
+}
---
+++ Projet S3/Assets/Script/Projection/Projection.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projection : MonoBehaviour
+{
+    private bool playerHere;
+    private GameObject playerContact;
+    private PlayerNumber playerNumber;
+
+    [Header("Caractéristiques de la projection")]
+    public float speedOfFly;
+    public float timeToFlight;
+    [Range(0, 1)] public float opportunityWindow;
+
+
+    void Start()
+    {
+        playerNumber = GetComponentInParent<PlayerNumber>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+
+        Jet();
+    }
+
+
+    public void ChangeState()
+    {
+        Vector3 dir = GetAimDirection();
+        if (dir != Vector3.zero)
+        {
+
+        }
+    }
+
+    public void Jet()
+    {
+        if (playerHere)
+        {
+            GetFly();
+            playerHere = false;
+        }
+    }
+
+    public Vector3 GetAimDirection()
+    {
+        Vector3 dir = Vector3.zero;
+        float horizontal = Input.GetAxis("AimHorizontal" + playerNumber.playerNumber.ToString());
+        float vertical = Input.GetAxis("AimVertical" + playerNumber.playerNumber.ToString());
+        dir = new Vector3(horizontal, 0, vertical).normalized;
+        return dir;
+    }
+
+    public void GetFly()
+    {
+        if (!playerContact.GetComponent<Fly>())
+        {
+            Fly flyScript = playerContact.AddComponent<Fly>();
+            flyScript.direction = GetAimDirection();
+            flyScript.timeToFlight = timeToFlight;
+            flyScript.speedOfFight = speedOfFly;
+            flyScript.opportunityWindow = opportunityWindow;
+        }
+    }
+
+    private void OnContact(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerHere = true;
+            playerContact = other.gameObject;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnContact(other);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        OnContact(other);
+    }
+
+}
