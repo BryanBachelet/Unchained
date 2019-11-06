24e8fa2ca1ec816293f8c9dd4aab157ed505d725
Endeavolt <49842893+Endeavolt@users.noreply.github.com>
2019-11-06 16:02:19 +0100

    Opportunity;

---
+++ Projet S3/Assets/Script/Opportunity.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opportunity : MonoBehaviour
+{
+    public bool activeInput;
+
+    private PlayerState playerState;
+
+    public void Start()
+    {
+        playerState = GetComponent<PlayerState>();
+    }
+
+    public void Update()
+    {
+        GainSpeed();
+    }
+
+    public void GainSpeed()
+    {
+        if (activeInput)
+        {
+            if (playerState.opportunityState == PlayerState.OpportunityState.In)
+            {
+                VitesseFunction.VitesseChange(true);
+            }
+            if (playerState.opportunityState == PlayerState.OpportunityState.Out)
+            {
+                VitesseFunction.VitesseChange(false);
+            }
+
+            activeInput = false;
+        }
+    }
+}
--- Projet S3/Assets/Script/PlayerState.cs
+++ Projet S3/Assets/Script/PlayerState.cs
@@ -6,5 +6,6 @@ public class PlayerState : MonoBehaviour
 {
     public enum StateOfPlayer { Free, Jet, Fly }
     public  StateOfPlayer playerState = StateOfPlayer.Free;
-    
+    public enum OpportunityState { In, Out }
+    public OpportunityState opportunityState = OpportunityState.Out;
 }
