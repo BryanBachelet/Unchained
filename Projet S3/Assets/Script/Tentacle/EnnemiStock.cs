9d3cef549c0a6eee5f8fa4ceb7459d92ccd74959
Endeavolt <49842893+Endeavolt@users.noreply.github.com>
2019-11-13 20:16:32 +0100

    Clean

--- Projet S3/Assets/Script/Tentacle/EnnemiStock.cs
+++ Projet S3/Assets/Script/Tentacle/EnnemiStock.cs
@@ -48,116 +48,27 @@ public class EnnemiStock : MonoBehaviour
         {
             lineRenderer.SetPosition(0, transform.position);
             lineRenderer.SetPosition(1, ennemiStock.transform.position);
-
-            if (Input.GetMouseButtonDown(0) && !rotate && !slam && !slamMe)
+            if (!slam && !rotateMe)
             {
-                rotate = rotationPlayer.StartRotation(ennemiStock, gameObject, "Ennemi", 60);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    rotate = rotationPlayer.StartRotation(ennemiStock, gameObject, "Ennemi", 60);
 
-            }
-            if (Input.GetMouseButtonDown(1) && !rotate && !slam && !slamMe)
-            {
-                rotate = rotationPlayer.StartRotation(gameObject, ennemiStock, "Player", 10);
+                }
+                if (Input.GetMouseButtonDown(1))
+                {
+                    rotate = rotationPlayer.StartRotation(gameObject, ennemiStock, "Player", 10);
 
-            }
-            //if (slam && !rotate)
-            //{
-            //    angleCompteur += Mathf.Abs(angleSpeed) * Time.deltaTime;
-            //    if (arriveOnSlam)
-            //    {
-            //        dir = ennemiStock.transform.position - gameObject.transform.position;
-            //        normal = Vector3.Cross(-dir, Vector3.up);
-            //        ennemyPosOnSlam = ennemiStock.transform.position;
-            //        myPosOnSlam = transform.position;
-            //        arriveOnSlam = false;
-            //    }
-            //    if (Input.GetKeyDown(KeyCode.A))
-            //    {
-            //        onDrop = true;
-            //    }
-            //    if (onDrop)
-            //    {
-            //        ennemiStock.transform.position = Vector3.MoveTowards(ennemiStock.transform.position, new Vector3(ennemiStock.transform.position.x, 0, ennemiStock.transform.position.z), 80 * Time.deltaTime);
-            //        if ((ennemiStock.transform.position - new Vector3(ennemiStock.transform.position.x, 0, ennemiStock.transform.position.z)).magnitude < 1)
-            //        {
-            //            onDrop = false;
-            //            angleCompteur = 180;
-            //        }
-            //    }
-            //    else if (angleCompteur < 180 && !onDrop)
-            //    {
-            //        ennemiStock.transform.RotateAround(myPosOnSlam, normal, -angleSpeed * Time.deltaTime);
-            //    }
-            //    else
-            //    {
-            //        Instantiate(slameAOEPrefab, ennemiStock.transform.position, transform.rotation);
-            //        ennemiStock.GetComponent<EnnemiDestroy>().isDestroying = true;
-            //        slam = false;
-            //        ennemiStock = null;
-            //        angleCompteur = 0;
-            //    }
+                }
 
-            //}
-            //else if (slamMe && !rotate && !rotateMe)
-            //{
-            //    angleCompteur += Mathf.Abs(angleSpeed) * Time.deltaTime;
-            //    if (arriveOnSlam)
-            //    {
-            //        dir = ennemiStock.transform.position - gameObject.transform.position;
-            //        normal = Vector3.Cross(dir, Vector3.up);
-            //        ennemyPosOnSlam = ennemiStock.transform.position;
-            //        myPosOnSlam = transform.position;
-            //        arriveOnSlam = false;
-            //    }
-            //    if (Input.GetKeyDown(KeyCode.E))
-            //    {
-            //        onDropMe = true;
-            //    }
-            //    if (onDropMe)
-            //    {
-            //        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, 0, transform.position.z), 80 * Time.deltaTime);
-            //        if ((transform.position - new Vector3(transform.position.x, 0, transform.position.z)).magnitude < 1)
-            //        {
-            //            onDropMe = false;
-            //            angleCompteur = 180;
-            //        }
-            //    }
-            //    else if (angleCompteur < 180 && !onDropMe)
-            //    {
-            //        if (angleCompteur > 20)
-            //        {
-            //            RaycastHit hit;
-            //            if (Physics.Raycast(transform.position, -Vector3.up, out hit, Mathf.Infinity, groundLayer))
-            //            {
-            //                Debug.Log((hit.point - transform.position).magnitude);
-            //                if ((hit.point - transform.position).magnitude < 0.1)
-            //                {
-            //                    angleCompteur = 180;
-            //                    transform.position = new Vector3(transform.position.x, 0, transform.position.z);
-            //                }
-            //                Debug.DrawRay(transform.position, -Vector3.up * Mathf.Infinity, Color.blue);
-            //            }
-
-            //        }
-            //        transform.RotateAround(ennemyPosOnSlam, normal, -angleSpeed * Time.deltaTime);
-            //    }
-
-            //    else
-            //    {
-            //        Instantiate(slameAOEPrefab, transform.position, transform.rotation);
-            //        ennemiStock.GetComponent<EnnemiDestroy>().isDestroying = true;
-            //        slamMe = false;
-            //        ennemiStock = null;
-            //        angleCompteur = 0;
-            //    }
-
-            //}
-            if (Input.GetKeyDown(KeyCode.A) && !slam)
-            {
-                slam = slamPlayer.StartSlam(ennemiStock, gameObject);
-            }
-            if (Input.GetKeyDown(KeyCode.E) && !slam)
-            {
-                slam = slamPlayer.StartSlam(gameObject, ennemiStock);
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    slam = slamPlayer.StartSlam(ennemiStock, gameObject);
+                }
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    slam = slamPlayer.StartSlam(gameObject, ennemiStock);
+                }
             }
         }
         else
