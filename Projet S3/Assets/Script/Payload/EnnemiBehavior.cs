8a5adccf096e412f195e40558c5ce2ee52bbaff8
Endeavolt <49842893+Endeavolt@users.noreply.github.com>
2019-11-13 11:47:56 +0100

    Rebuild Louis

--- Projet S3/Assets/Script/Payload/EnnemiBehavior.cs
+++ Projet S3/Assets/Script/Payload/EnnemiBehavior.cs
@@ -5,22 +5,49 @@ using UnityEngine;
 public class EnnemiBehavior : MonoBehaviour
 {
 
-   public GameObject target;
+    public GameObject target;
+    public GameObject currentTarget;
     public float speed;
     public bool isOnSlam;
+    private AgentFaction agentFaction;
+    private bool faction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (GetComponentInParent<AgentFaction>())
+        {
+            agentFaction = GetComponent<AgentFaction>();
+        }
+        else
+        {
+            faction = false;
+        }
+        currentTarget = target;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!isOnSlam)
+        if(currentTarget == null)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+            currentTarget = target; 
+        }
+        if (!isOnSlam)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget.transform.position, speed * Time.deltaTime);
         }
 
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (faction)
+        {
+            if (collision.gameObject.GetComponent<AgentFaction>() && collision.gameObject.GetComponent<AgentFaction>().factions != agentFaction.factions)
+            {
+                collision.gameObject.GetComponent<EnnemiDestroy>().isDestroying = true;
+            }
+        }
+    }
 }
+
