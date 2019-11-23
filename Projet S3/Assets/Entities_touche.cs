e949e98e350be18efd43783cea3fef1cc134e841
Endeavolt <49842893+Endeavolt@users.noreply.github.com>
2019-11-23 16:16:55 +0100

    Son Merge

---
+++ Projet S3/Assets/Entities_touche.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entities_touche : MonoBehaviour
+{
+    Animator anim;
+  
+
+        private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        
+            if (other.gameObject.tag == "Bullet")
+            {
+                anim.SetBool("Attrapé", true);
+
+        }
+
+        
+    }
+
+}
