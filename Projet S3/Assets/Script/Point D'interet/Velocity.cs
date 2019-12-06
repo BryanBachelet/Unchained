05380d66f956fd94db58246ebaee26e31cce9828
Endeavolt <49842893+Endeavolt@users.noreply.github.com>
2019-12-06 11:16:25 +0100

    Velocity V1

--- Projet S3/Assets/Script/Point D'interet/Velocity.cs
+++ Projet S3/Assets/Script/Point D'interet/Velocity.cs
@@ -4,26 +4,74 @@ using UnityEngine;
 
 public class Velocity : MonoBehaviour
 {
+    [Header("Projection Strengh Gain")]
     public float velocityStatOne;
-    public float velocityStatTwo;
-    public float velocityStatThree;
+    public float convertisseurStatOne = 10;
+    public float realPointOne;
 
-    public float DecreaseOfVelocity;
+    [Header("V2")]
+    public float velocityStatTwo;
+    public float convertisseurStatTwo = 10;
+    public float realPointTwo;
+
+    [Header("V3")]
+    public float velocityStatThree = 0;
+    public float convertisseurStatThree = 10;
+    public float realPointThree ;
+
+    [Header("General Stat")]
+    public float DecreaseOfTimerVelocity = 2;
+    public float DecreaseOfVelocity = 1;
 
     private float compteurOne;
     private float compteurTwo;
     private float compteurThree;
 
+    private EnnemiStock ennemiStock;
+    private PlayerMoveAlone playerMove;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        playerMove = GetComponent<PlayerMoveAlone>();
+        ennemiStock = GetComponent<EnnemiStock>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        realPointOne = velocityStatOne / convertisseurStatOne;
+        ennemiStock.powerOfProjection = playerMove.powerOfProjection + realPointOne;
+        Debug.Log(playerMove.powerOfProjection + realPointOne);
+        realPointTwo = velocityStatTwo / convertisseurStatTwo;
+        realPointThree = velocityStatThree / convertisseurStatThree;
+
+        if (compteurOne > DecreaseOfTimerVelocity)
+        {
+            velocityStatOne -= DecreaseOfVelocity * Time.deltaTime;
+        }
+        else
+        {
+            compteurOne += Time.deltaTime;
+        }
+        if (compteurTwo > DecreaseOfTimerVelocity)
+        {
+            velocityStatTwo -= DecreaseOfVelocity * Time.deltaTime;
+        }
+        else
+        {
+            compteurTwo += Time.deltaTime;
+        }
+        if (compteurThree > DecreaseOfTimerVelocity)
+        {
+            velocityStatThree -= DecreaseOfVelocity * Time.deltaTime;
+        }
+        else
+        {
+            compteurThree += Time.deltaTime;
+        }
+
     }
 
     public void GetAddVelocityPoint(int i)
