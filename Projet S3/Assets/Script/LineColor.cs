f2146456449daf712aca4565c816db4e703f913a
Bryan Bachelet <bryanbacheletpro@gmail.com>
2020-01-22 20:45:52 +0100

    Line Changement de couleur

--- Projet S3/Assets/Script/LineColor.cs
+++ Projet S3/Assets/Script/LineColor.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class LineColor : MonoBehaviour
 {
     public LineRenderer line;
+    public Color colorShoot;
+    public Color colorRotate;
+    public float speedOfChangeColor = 1;
+
+    private Color currentColor;
+    private float t = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +20,28 @@ public class LineColor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(StateAnim.state != StateAnim.CurrentState.Rotate)
+        {
+            ChangeColor(colorShoot);
+
+        }
+        else
+        {
+            ChangeColor(colorRotate);
+        }
+        line.material.SetColor("_EmissionColor", currentColor);
+    }
+
+   public void ChangeColor(Color colorTaget)
+    {
+        if (currentColor != colorTaget)
+        {
+            currentColor = Color.Lerp(currentColor, colorTaget, t);
+            t = speedOfChangeColor * Time.deltaTime;
+        }
+        else
+        {
+            t = 0;
+        }
     }
 }
