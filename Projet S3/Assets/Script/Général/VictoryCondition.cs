cdab1e3ffd18222859c81ac9bcc6a118fe0df09b
Bryan Bachelet <bryanbacheletpro@gmail.com>
2020-02-13 21:19:10 +0100

    Victory Condition

---
+++ Projet S3/Assets/Script/Général/VictoryCondition.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class VictoryCondition : MonoBehaviour
+{
+    public int nextSceneToLoad;
+    public bool restartSameScene = true;
+    public GameObject victoryText;
+
+    private bool victoryRestart = false;
+    public float timeBeforeRestart = 2f;
+    private float compteurRestart = 0;
+
+    public void Update()
+    {
+        if (victoryRestart)
+        {
+            if (compteurRestart > timeBeforeRestart)
+            {
+                if (!restartSameScene)
+                {
+                    SceneManager.LoadScene(nextSceneToLoad);
+                }
+                else
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
+            }
+            else
+            {
+                compteurRestart += Time.deltaTime;
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ActiveVictory();
+        }
+    }
+
+    public void ActiveVictory()
+    {
+        victoryRestart = true;
+        victoryText.SetActive(true);
+    }
+}
