cb1961c26f9acb3c78834f16ca753cec909738b9
Guilleade <titoug56@gmail.com>
2020-02-27 11:28:03 +0100

    Recup Bryan commit

---
+++ Projet S3/Assets/Script/Player/Explosion.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public int range;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            ExplosionTransformation();
+        }
+    }
+
+    public void ExplosionTransformation()
+    {
+        Collider[] entityInRange = Physics.OverlapSphere(transform.position, range);
+        for(int i = 0; i < entityInRange.Length; i++)
+        {
+            entityInRange[i].GetComponent<Rigidbody>().AddForce(Vector3.right);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
