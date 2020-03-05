5ad3349b6e57a3eff90b1b160d86dda27598f41c
Bryan Bachelet <bryanbacheletpro@gmail.com>
2020-03-05 11:29:34 +0100

    Package Selma

---
+++ Projet S3/Assets/Editor/CustomPlanCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlanCamera))]
+[CanEditMultipleObjects]
+public class CustomPlanCamera : Editor
+{
+    public SerializedProperty myTarget;
+
+    public void OnEnable()
+    {
+        myTarget = serializedObject.FindProperty("activeGameObject");   
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        PlanCamera plan = (PlanCamera)target;
+        GUILayout.Space(10);
+
+        Debug.Log(myTarget);
+        myTarget.arraySize = EditorGUILayout.IntField("Test 1", myTarget.arraySize);
+
+
+        if (EditorGUILayout.PropertyField(myTarget, true))
+        {
+           
+
+
+        }
+        GUILayout.Space(10);
+        serializedObject.ApplyModifiedProperties();
+
+        if (GUILayout.Button("Test"))
+        {
+            plan.Addition();
+        }
+    }
+}
---
+++ Projet S3/Assets/Script/Misc/Test Bryan/PlanCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanCamera : MonoBehaviour
+{
+    public int i = 3;
+
+    public TestStuck[] activeGameObject = new TestStuck[1];
+
+
+    public void Addition()
+    {
+        Debug.Log(activeGameObject[0].numberOfTarget);
+        Debug.Log(i * 2);
+    }
+    [System.Serializable]
+    public struct TestStuck
+    {
+        public GameObject myTarget;
+        public int numberOfTarget;
+    };
+}
