7151dbec21613e4477e322e50d4a4b8e3dcf22ef
Guilleade <titoug56@gmail.com>
2020-03-10 17:55:38 +0100

    Mash Logic + transformation effect

--- Projet S3/Assets/Script/Camera/TransformationCam.cs
+++ Projet S3/Assets/Script/Camera/TransformationCam.cs
@@ -15,6 +15,12 @@ public class TransformationCam : MonoBehaviour
     private float dist;
     private bool activeBehavior;
     private float angleCompteur;
+
+    public float shake_decay = 0;
+    public float shake_intensity = 0;
+
+    Quaternion originRotation;
+    public float clampValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,20 +30,35 @@ public class TransformationCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if(cam.i == cam.cams.Count && StateOfGames.currentState == StateOfGames.StateOfGame.Transformation)
         {
+            originRotation = transform.rotation;
             if (!activeBehavior)
             {
                 basePos = startPos.transform.position;
                 dir = (transform.position - player.transform.position).normalized;
                 dist = (transform.position - player.transform.position).magnitude;
             }
+            if (shake_intensity > 0)
+            {
+                transform.rotation = Quaternion.Euler(originRotation.x + Random.Range(-shake_intensity, shake_intensity) * 0.2f,
+                                originRotation.y + Random.Range(-shake_intensity, shake_intensity) * 0.2f,
+                                originRotation.z + Random.Range(-shake_intensity, shake_intensity) * 0.2f);
+                shake_intensity -= shake_decay;
+            }
+            Shake();
             Vector3 nextPos = player.transform.position + Quaternion.Euler(0, angleCompteur, 0) * (dir * dist);
             transform.position = Vector3.Lerp(transform.position, nextPos, 1);
             transform.LookAt(player.transform.position);
             angleCompteur += speedOfRotation * Time.deltaTime;
-            angleCompteur = Mathf.Clamp(angleCompteur, 0, 5);
+            angleCompteur = Mathf.Clamp(angleCompteur, 0, clampValue);
+
         }
         
+    }
+    void Shake()
+    {
+        originRotation = transform.rotation;
     }
 }
--- Projet S3/Assets/Script/Player/MashingTrans.cs
+++ Projet S3/Assets/Script/Player/MashingTrans.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class MashingTrans : MonoBehaviour
 {
-    public int i;
-    public float timing;
     public int numberToAim;
+    public int numberInput;
+    List<float> i = new List<float>();
+    public float timing;
 
     public Text text;
     public CamMouvement camMouvement;
@@ -16,6 +17,8 @@ public class MashingTrans : MonoBehaviour
     private TransformationAgent agentTransfo;
     private ResetPlayer resetPlayerScript;
     private bool activeExplode;
+
+    Collider[] hitColliders;
     void Start()
     {
         resetPlayerScript = GetComponent<ResetPlayer>();
@@ -24,43 +27,48 @@ public class MashingTrans : MonoBehaviour
 
     void Update()
     {
+        hitColliders = Physics.OverlapSphere(transform.position, 3);
+        
+        for(int j = 0; j < i.Count; j++)
+        {
+            if (i[j] + 1 < Time.time)
+            {
+                i.RemoveAt(j);
+            }
+
+        }
+        numberInput = i.Count;
+        numberToAim = hitColliders.Length;
         if (camMouvement.i >= camMouvement.cams.Count)
         {
-            text.gameObject.SetActive(true);
-
+            if(hitColliders.Length > 7)
+            {
+                compteur += Time.deltaTime;
+            }
+                text.gameObject.SetActive(true);
             if (Input.GetKeyDown(KeyCode.Joystick1Button0))
             {
-                i++;
+                i.Add(Time.time);
             }
-            if (compteur > timing)
+            agentTransfo.startTranformationAnim(timing);
+            if (i.Count < hitColliders.Length / 3 && compteur > 0.5f)
             {
-                if (i > numberToAim)
-                {
-                    Debug.Log("Win");
-                    if (!activeExplode)
-                    {
-                        text.gameObject.SetActive(false);
-                        agentTransfo.ExploseAgent();
-                        activeExplode = true;
-                    }
-                    if (compteur > timing + 1)
-                    {
-                        StateOfGames.currentState = StateOfGames.StateOfGame.DefaultPlayable;
-                    }
-
-                }
-                else
+                text.gameObject.SetActive(false);
+                resetPlayerScript.ResetFonction(true);
+            }
+            else if(i.Count > hitColliders.Length / 1.2 && hitColliders.Length > 7)
+            {
+                Debug.Log("Win");
+                if (!activeExplode)
                 {
                     text.gameObject.SetActive(false);
-                    resetPlayerScript.ResetFonction(true);
-                    Debug.Log("Lose");
+                    agentTransfo.ExploseAgent();
+                    activeExplode = true;
+                    StateOfGames.currentState = StateOfGames.StateOfGame.DefaultPlayable;
                 }
+
             }
-            else
-            {
-                agentTransfo.startTranformationAnim(timing);
-            }
-                compteur += Time.deltaTime;
+
         }
 
     }
