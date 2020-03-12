90b8c4fe29ea8860304871a62193a956edd2f0ba
Bryan Bachelet <bryanbacheletpro@gmail.com>
2020-03-12 15:33:09 +0100

    Fix Bug and Clean Code

--- Projet S3/Assets/Script/Camera/CameraAction.cs
+++ Projet S3/Assets/Script/Camera/CameraAction.cs
@@ -7,61 +7,21 @@ public class CameraAction : MonoBehaviour
 
     [HideInInspector] public GameObject player;
 
-    public GameObject posCamera;
-
-    [Header("Options")] public bool StatiqueCam;
-
-    [Header("Bullet")]
-    public float distanceOfStartDezoomBullet = 10;
-    public float speedDezoomBullet;
-
-    [Header("Agent")]
-    public float distanceOfStartDezoomAgent = 20;
-    public float speedDezoomAgent;
-
-    [Header("Zoom")]
-    public float speedZoomSpeed;
-
-
-    [Header("Proposition")]
-    public bool decalageScope;
-    public float decalageCamera = 0;
-    public float speedDecalage = 10;
-    public float multiplicateurSpeedMax = 5;
-    [Range(0, 1)]
-    public float resetLerp = 0.3f;
-    private Vector3 previousMousePos;
-
-    public float distanceMax = 100;
-    private bool supZero;
     [HideInInspector] public Vector3 ecartJoueur;
+    private bool activeBehavior;
     [HideInInspector] public Vector3 basePosition;
-    private MouseScope playerMouseScope;
-    private EnnemiStock playerEnnemiStock;
-    private float distanceBullet;
-    private float distanceAgent;
-    public float compteurDezoomBullet;
-    private float compteurZoomBullet;
-    private float competeur;
+    
     public Camera orthoCam;
-    public PlayerMoveAlone moveAlone;
-    public float pointFocusCame = 1;
-
-    private bool activeBehavior =false;
-    private Vector3 currentDir = Vector3.zero;
-    private Vector3 rotate;
-    public bool newBehavior = true;
+  
+    
     // Start is called before the first frame update
     void Start()
     {
         player = PlayerMoveAlone.Player1;
-        moveAlone = player.GetComponent<PlayerMoveAlone>();
+       
 
         ecartJoueur = transform.position - player.transform.position;
-       
-        playerMouseScope = player.GetComponent<MouseScope>();
-        playerEnnemiStock = player.GetComponent<EnnemiStock>();
-        previousMousePos = transform.position;
+     
     }
 
 
@@ -88,125 +48,6 @@ public class CameraAction : MonoBehaviour
          
         }
 
-        if (!StatiqueCam)
-        {
-
-            if (playerMouseScope.instanceBullet != null || playerEnnemiStock.ennemiStock != null)
-            {
-                if (playerMouseScope.instanceBullet != null)
-                {
-                    distanceBullet = Vector3.Distance(player.transform.position, playerMouseScope.instanceBullet.transform.position);
-                    if (distanceBullet > distanceOfStartDezoomBullet)
-                    {
-                        compteurDezoomBullet += Time.deltaTime / speedDezoomBullet;
-
-                        float currentDezoom = Mathf.Clamp(((distanceBullet - 10) / 1.5f), 0, distanceMax);
-                        Vector3 camPos = basePosition + -transform.forward * currentDezoom;
-                        transform.position = Vector3.Lerp(transform.position, camPos, compteurDezoomBullet);
-                    }
-                    else
-                    {
-                        transform.position = Vector3.Lerp(transform.position, basePosition, compteurZoomBullet);
-                        compteurZoomBullet += Time.deltaTime;
-                    }
-                    competeur = 0;
-                }
-                if (playerEnnemiStock.ennemiStock != null)
-                {
-                    distanceAgent = Vector3.Distance(player.transform.position, playerEnnemiStock.ennemiStock.transform.position);
-
-                    Vector3 newPos = Vector3.zero;
-                    if (!newBehavior)
-                    {
-                        Vector3 dir = playerEnnemiStock.ennemiStock.transform.position - player.transform.position;
-                        if (distanceAgent > distanceOfStartDezoomAgent)
-                        {
-
-                            compteurDezoomBullet += Time.deltaTime / speedDezoomAgent;
-
-                            Vector3 camPos = basePosition + -transform.forward * (distanceAgent / 1f);
-                            transform.position = Vector3.Lerp(transform.position, camPos, compteurDezoomBullet);
-                        }
-                        else
-                        {
-                            compteurZoomBullet += Time.deltaTime;
-                            transform.position = Vector3.Lerp(transform.position, basePosition, compteurZoomBullet);
-
-                        }
-
-                        dir = new Vector3(dir.x, 0, dir.z);
-                        float dot = Vector3.Dot(dir.normalized, player.transform.forward);
-                        pointFocusCame = distanceAgent / 50;
-                        pointFocusCame = Mathf.Clamp(pointFocusCame, 0f, 1);
-
-                        newPos = transform.position + dir.normalized * (distanceAgent * (pointFocusCame + dot));
-                    }
-                    else
-                    {
-                        if (distanceAgent > distanceOfStartDezoomAgent)
-                        {
-
-                            newPos = playerEnnemiStock.ennemiStock.transform.position + -transform.forward * (distanceAgent * 1.5f);
-                        }
-                        else
-                        {
-                            newPos = playerEnnemiStock.ennemiStock.transform.position + -transform.forward * (distanceOfStartDezoomAgent * 1.5f);
-                        }
-                    }
-                    transform.position = Vector3.Lerp(transform.position, newPos, competeur);
-                    competeur += Time.deltaTime;
-                    compteurZoomBullet = 0;
-
-                }
-
-            }
-            else
-            {
-                if (StateAnim.state != StateAnim.CurrentState.Projection)
-                {
-                    Vector3 checkDir = (playerMouseScope.direction + playerMouseScope.directionManette).normalized;
-                    basePosition = player.transform.position + ecartJoueur;
-                    Vector3 newPos = basePosition;
-                    if (compteurZoomBullet >= 1)
-                    {
-                        compteurZoomBullet = 0;
-                    }
-
-                    if (checkDir != Vector3.zero)
-                    {
-                        float dirDot = Vector3.Dot(checkDir, currentDir);
-                        Debug.DrawRay(player.transform.position, checkDir * 100, Color.blue);
-                        if (dirDot < 0.85f)
-                        {
-                            currentDir = checkDir;
-                            compteurZoomBullet = 0;
-                        }
-                        float orientationOnScreen = Mathf.Abs(Mathf.Clamp(0f, -1f, checkDir.z - 1));
-                        newPos = basePosition + (checkDir * decalageCamera * orientationOnScreen);
-                        transform.position = Vector3.Lerp(transform.position, newPos, compteurZoomBullet);
-                        compteurZoomBullet += Time.deltaTime * speedDecalage;
-                    }
-                    else
-                    {
-                        transform.position = newPos;
-                    }
-                    compteurDezoomBullet = 0;
-                    competeur = 0;
-                    Debug.DrawRay(player.transform.position, currentDir * 100, Color.green);
-                }
-                else
-                {
-                    transform.position = Vector3.Lerp(transform.position, basePosition, compteurZoomBullet);
-                    compteurZoomBullet += Time.deltaTime * speedDecalage;
-                }
-
-
-
-            }
-            //float clampHeightZ = Mathf.Clamp(transform.position.z,basePosition.z+ -30 , basePosition.z);
-            //float clampHeightY = Mathf.Clamp(transform.position.y, basePosition.y, basePosition.y +20);
-            //transform.position = new Vector3(transform.position.x, clampHeightY, clampHeightZ);
-        }
     }
 
 }
--- Projet S3/Assets/Script/Player/MouseScope.cs
+++ Projet S3/Assets/Script/Player/MouseScope.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class MouseScope : MonoBehaviour
 {
 
-
-    public Material line;
+    [Header("Fonctionnement")]
     public GameObject bullet;
+    public RectTransform directionIMG;
+    private Vector3 posConvert;
+    public GameObject uIGOAim;
+
+    
+    [Header("Debug")]
+    public Material line;
     [HideInInspector] public GameObject Ambout;
 
     private EnnemiStock ennemiStock;
     [HideInInspector] public Vector3 direction;
     [HideInInspector] public Vector3 directionManette;
-    public GameObject instanceBullet;
+    [HideInInspector] public GameObject instanceBullet;
     private LineRenderer lineRenderer;
     private bool returnLine;
     private bool destructBool;
@@ -26,7 +32,7 @@ public class MouseScope : MonoBehaviour
     public float speedOfBullet;
     public float timeBetweenShoot = 0.4f;
     private float _timerOfBullet;
-    public float returnSpeed = 50;
+    
     public float distanceMaxOfShoot = 75;
     private GameObject meshBullet;
     Projectils projectils;
@@ -34,6 +40,11 @@ public class MouseScope : MonoBehaviour
     public bool distanceDestruct;
     public bool activePC;
     public bool activeSnap;
+    [Header("Snap")]
+    public LayerMask layerMask;
+    [Range(0.5f, 1)]
+    public float angleSnap = 0.75f;
+    public Color colorSnap;
     [Header("Retour Sound")]
     [FMODUnity.EventRef]
     public string returnSound;
@@ -50,9 +61,6 @@ public class MouseScope : MonoBehaviour
     private bool snapDeactive;
     private GameObject entitySnap;
 
-    public RectTransform directionIMG;
-    public Vector3 posConvert;
-    public GameObject uIGOAim;
     private float frame;
     // Start is called before the first frame update
     void Start()
@@ -65,7 +73,7 @@ public class MouseScope : MonoBehaviour
         returnEvent = FMODUnity.RuntimeManager.CreateInstance(returnSound);
         returnEvent.setVolume(returnVolume);
         speedOfBullet = distanceMaxOfShoot / timerOfBullet;
-        returnSpeed = distanceMaxOfShoot / timeBetweenShoot;
+       
     }
 
     // Update is called once per frame
@@ -143,14 +151,9 @@ public class MouseScope : MonoBehaviour
                         {
                             Destroy(instanceBullet);
                             lineRenderer.SetPosition(0, transform.position);
-                            //ReturnState();
+                         
                         }
-                        //else
-                        //{
-                        //    ReturnOrientation();
-                        //}
-
-
+                      
                     }
                     else
                     {
@@ -162,60 +165,19 @@ public class MouseScope : MonoBehaviour
                     if (Vector3.Distance(transform.position, instanceBullet.transform.position) >= distanceMaxOfShoot && !projectils.returnBall)
                     {
                         Destroy(instanceBullet);
-
-                        // ReturnState();
                     }
-                    if (projectils.returnBall)
-                    {
-                        //ReturnOrientation();
-                    }
+                 
                 }
             }
 
-            //returnLine = true;
-            //destructBool = false;
+            
 
             ballPos = instanceBullet.transform.position;
 
         }
 
-        if (ennemiStock.ennemiStock == null && instanceBullet == null)
-        //Return de line renderer après le tir;
-        {
-            if (returnLine)
-            {
-                if (!destructBool)
-                {
-                    returnPos = ballPos;
-                    returnEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
-
-                    returnEvent.start();
-                    destructBool = true;
-                }
-
-
-                returnLine = false;
-                if (lineRenderer.enabled)
-                {
-                    lineRenderer.enabled = false;
-                }
-
-
-            }
-        }
-        if (ennemiStock.ennemiStock == null && instanceBullet != null)
-        {
-
-            if (projectils.returnBall)
-            {
-                projectils.dir = -(projectils.transform.position - transform.position);
-                if (Vector3.Distance(transform.position, instanceBullet.transform.position) < returnSpeed * Time.deltaTime && snapDeactive)
-                {
-                    DestroyBullet();
-
-                }
-            }
-        }
+       
+       
     }
 
 
@@ -229,7 +191,7 @@ public class MouseScope : MonoBehaviour
         }
         Ray rayCheckEntity = new Ray(transform.position, directionManette.normalized);
         RaycastHit hitEntity = new RaycastHit();
-        if (Physics.Raycast(rayCheckEntity, out hitEntity, distanceMaxOfShoot, 1<<9))
+        if (Physics.Raycast(rayCheckEntity, out hitEntity, distanceMaxOfShoot, layerMask))
         {
             if (active)
             {
@@ -242,14 +204,14 @@ public class MouseScope : MonoBehaviour
             }
 
             entitySnap = hitEntity.collider.gameObject;
-            entitySnap.GetComponent<MeshRenderer>().material.color = Color.yellow;
+            entitySnap.GetComponent<MeshRenderer>().material.color = colorSnap;
 
 
         }
         else
         {
             Vector3 posHit = transform.position;
-            Collider[] pos = Physics.OverlapSphere(posHit, distanceMaxOfShoot, 1 << 9);
+            Collider[] pos = Physics.OverlapSphere(posHit, distanceMaxOfShoot, layerMask);
             float disMin = distanceMaxOfShoot;
             int entity = 0;
             for (int i = 0; i < pos.Length; i++)
@@ -257,7 +219,7 @@ public class MouseScope : MonoBehaviour
                 float currentdist = Vector3.Distance(posHit, pos[i].transform.position);
                 Vector3 dirEntity = pos[i].transform.position - transform.position;
 
-                if (currentdist < disMin && Vector3.Dot(directionManette.normalized, dirEntity.normalized) > 0.75f)
+                if (currentdist < disMin && Vector3.Dot(directionManette.normalized, dirEntity.normalized) > angleSnap)
                 {
 
                     disMin = currentdist;
@@ -272,7 +234,7 @@ public class MouseScope : MonoBehaviour
             if (entity != 0 && disMin != distanceMaxOfShoot)
             {
                 entitySnap = pos[entity].gameObject;
-                entitySnap.GetComponent<MeshRenderer>().material.color = Color.yellow;
+                entitySnap.GetComponent<MeshRenderer>().material.color = colorSnap;
             }
 
             if (active)
@@ -326,7 +288,7 @@ public class MouseScope : MonoBehaviour
             GL.Vertex(transform.position);
             GL.Vertex(transform.position + (Quaternion.Euler(0, -24, 0) * (direction + directionManette).normalized) * distanceMaxOfShoot);
             GL.End();
-
+            
         }
 
     }
@@ -336,18 +298,12 @@ public class MouseScope : MonoBehaviour
         Gizmos.color = Color.black;
         Gizmos.DrawRay(transform.position, (direction + directionManette).normalized * distanceMaxOfShoot);
     }
-    private void ReturnState()
-    {
-        projectils.returnBall = true;
-        projectils.dir = -projectils.dir;
-        projectils.speed = returnSpeed;
-    }
+   
     private void ReturnOrientation()
     {
         projectils.dir = transform.position - instanceBullet.transform.position;
         float angle = Vector3.SignedAngle(transform.forward, projectils.dir, transform.up);
-        //Vector3 eulers = new Vector3(meshBullet.transform.eulerAngles.x, angle, meshBullet.transform.eulerAngles.z);
-        // meshBullet.transform.localRotation = Quaternion.Euler(eulers);
+        
     }
 
 
@@ -358,17 +314,13 @@ public class MouseScope : MonoBehaviour
 
             StateAnim.ChangeState(StateAnim.CurrentState.Tir);
             instanceBullet = Instantiate(bullet, transform.position + (dir).normalized, Quaternion.identity);
-            //  meshBullet = Instantiate(Ambout, instanceBullet.transform.position, Quaternion.identity, instanceBullet.transform);
-            float angle = Vector3.SignedAngle(transform.forward, (direction + directionManette).normalized, transform.up);
-
-            // Vector3 eulers = new Vector3(Ambout.transform.eulerAngles.x, angle, Ambout.transform.eulerAngles.z);
-            // meshBullet.transform.localRotation = Quaternion.Euler(eulers);
+           
 
             _timerOfBullet = 0;
 
 
             projectils = instanceBullet.GetComponent<Projectils>();
-            projectils.dir = dir.normalized; //(direction + directionManette).normalized;
+            projectils.dir = dir.normalized; 
             projectils.player = gameObject;
             projectils.lineRenderer = lineRenderer;
             projectils.speed = speedOfBullet;
@@ -403,12 +355,12 @@ public class MouseScope : MonoBehaviour
         Ray camera = Camera.main.ScreenPointToRay(uIGOAim.transform.position);
         RaycastHit hit;
         LayerMask mask = ~(1 << 11);
-        Debug.DrawRay(Camera.main.transform.position, camera.direction * 100);
+
         if (Physics.Raycast(camera, out hit, Mathf.Infinity, mask))
         {
             posConvert = hit.point + Vector3.up;
         }
-        Debug.DrawRay(Camera.main.transform.position, camera.direction * hit.distance);
+       
         float aimHorizontal = Input.GetAxis("AimHorizontal1");
         float aimVertical = -Input.GetAxis("AimVertical1");
 
