9ddc38ba9e9de8e4432a134c4eaf8d8de5c35214
Bryan Bachelet <bryanbacheletpro@gmail.com>
2020-03-18 17:50:21 +0100

    Valid Merge

--- Projet S3/Assets/Script/Entities/CircleFormation.cs
+++ Projet S3/Assets/Script/Entities/CircleFormation.cs
@@ -50,7 +50,7 @@ public class CircleFormation : MonoBehaviour
         }
         if (doDestruct)
         {
-            Debug.Break();
+           
             Destroy(gameObject);
         }
     }
