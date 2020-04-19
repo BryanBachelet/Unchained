123f869b5b5cf4a5432622fb8a517a7d85cdea5c
Guilleade <titoug56@gmail.com>
2020-04-19 14:58:04 +0200

    MAJ scriptVfx

--- Projet S3/Assets/VfxTrailOnPlayer.cs
+++ Projet S3/Assets/VfxTrailOnPlayer.cs
@@ -6,17 +6,19 @@ public class VfxTrailOnPlayer : MonoBehaviour
 {
     Transform playerTransform;
     PlayerMoveAlone pmaPlayer;
+    EnnemiStock esPlayer;
     // Start is called before the first frame update
     void Start()
     {
         playerTransform = PlayerMoveAlone.Player1.transform;
         pmaPlayer = playerTransform.GetComponent<PlayerMoveAlone>();
+        esPlayer = playerTransform.GetComponent<EnnemiStock>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(pmaPlayer.currentPowerOfProjection > 0)
+        if(pmaPlayer.currentPowerOfProjection > 0 || esPlayer.ennemiStock != null)
         {
             transform.GetChild(0).gameObject.SetActive(true);
         }
@@ -24,6 +26,8 @@ public class VfxTrailOnPlayer : MonoBehaviour
         {
             transform.GetChild(0).gameObject.SetActive(false);
         }
+
+
         transform.position = new Vector3(playerTransform.position.x, 2, playerTransform.position.z);
     }
 }
