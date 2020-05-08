651f4514c655b317a8cecfc452f502aaf4423cc2
Bryan Bachelet <bryanbacheletpro@gmail.com>
2020-05-08 17:22:37 +0200

    Avatar Anim First Part

---
+++ Projet S3/Assets/Script/Player/PlayerAnimState.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimState : MonoBehaviour
+{
+    public enum PlayerStateAnim {Idle,Shoot, Rotation, Projection,Entrave}
+    public PlayerStateAnim playerCurrentState;
+
+    public Animator animator;
+    public bool right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    public void ChangeStateAnim(PlayerStateAnim currentState)
+    {
+        if(playerCurrentState != currentState)
+        {
+            if(currentState == PlayerStateAnim.Idle)
+            { 
+                if(playerCurrentState == PlayerStateAnim.Projection )
+                {
+                    animator.Play("Anim_State_Chaine_Projection releve");
+                }
+                else
+                {
+
+                    animator.Play("Anim_State_Chaine_Idle");
+                }
+            }
+            if(currentState == PlayerStateAnim.Shoot)
+            {
+                if(right)
+                {
+                    animator.Play("Anim_State_Chaine_LancéDroit");
+                }
+                else
+                {
+                    animator.Play("Anim_State_Chaine_LancéGauche");
+                }
+            }
+            if(currentState == PlayerStateAnim.Rotation)
+            {
+                animator.Play("Anim_State_Chaine_StartRotate");
+            }
+                if(currentState == PlayerStateAnim.Projection)
+            {
+                    animator.Play("Anim_State_Chaine_ProjectionStart");
+            }
+            playerCurrentState = currentState;
+        }
+    }
+}
